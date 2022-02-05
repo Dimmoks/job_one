ce591f355211c773a01921faa53d41259b3a54c9
Dima Karasev <dima2007crossout@gmail.com>
2022-02-05 18:54:06 +0300

    lesson29

---
+++ lesson/Lesson 29/script/script.js
@@ -0,0 +1,21 @@
+//это тревожит
+alert ('Добро пожаловать!');
+
+
+/*Это 
+не 
+тревожит*/ 
+console.log('Это лог, честно.');
+
+let x = 'hello',
+    probel = '',
+    y = 'world',
+    z;
+
+z = x + probel + y;
+
+let a = 8;
+
+z = z + a;
+
+console.log(z);
