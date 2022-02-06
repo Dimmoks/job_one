e5a46eb0f3d1e3d7005a77249069888b3c8c8801
Dima Karasev <dima2007crossout@gmail.com>
2022-02-06 11:32:34 +0300

    Lesson30

---
+++ lesson/Lesson 30/script/script.js
@@ -0,0 +1,19 @@
+let str = "Привет";
+let str2 = "Как дела?";
+
+console.log(str);
+console.log(str2);
+
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+let isGreater = 4 > 1;
+alert( isGreater );
+
+let x = 5,
+    y = 5;
+
+console.log(typeof(y));
+x = Number(y);
+console.log(typeof(y));
+console.log(x + y)
